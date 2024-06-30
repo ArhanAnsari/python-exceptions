b0c581bc1cd08c95bc92ae0c04050d313b091d09
Arhan Ansari <arhanansari2009@gmail.com>
2024-06-30 18:14:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,36 @@ print(float(b))
 name = "Arhan"
 num = 4
 print(name + num + name)
+
+#Index Error
+j = [1, 2, 4]
+print(j[4])
+
+my_string = "CodeWithArhan"
+print(my_string[-61])
+
+#Try Except Block
+try:
+ x = 1/0
+except:
+ print("Division By Zero")
+
+#Else and Finally Block
+try:
+ y = 1/1
+except ZeroDivisionError:
+ print("You can't divide by zero!")
+else:
+ print("No exceptions were raised")
+finally:
+ print("This block runs no matter what.")
+
+#Custom Exceptions
+
+class MyError(Exception):
+ pass
+
+try:
+ raise MyError("Something went wrong!")
+except MyError as e:
+ print(e)
